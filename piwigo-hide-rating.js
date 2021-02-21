ac8cedb4e33a55e4cab760bc50c53407b6a600b9
Andreas <baumrasen@users.noreply.github.com>
2021-02-21 10:36:35 +0100

    Update piwigo-hide-rating.js

--- piwigo-hide-rating.js
+++ piwigo-hide-rating.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         Piwigo-hide-rating
 // @namespace    https://github.com/baumrasen/userscripts
-// @version      0.2
+// @version      0.3
 // @description  hide the rating div in piwigo, if you still rated the image
 // @author       baumrasen
 // @include      http*://*/picture.php?/*
 // @downloadURL  https://raw.githubusercontent.com/baumrasen/userscripts/main/piwigo-hide-rating.js
 // @updateURL    https://raw.githubusercontent.com/baumrasen/userscripts/main/piwigo-hide-rating.js
+// @supportURL   https://github.com/baumrasen/userscripts/issues
 // @grant        none
 // ==/UserScript==
 
